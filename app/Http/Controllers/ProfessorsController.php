5e7bb8430a82ab1766914187f7ec7127d4c1118e
Felipe Santana D`Eça Almeida <felipe.almeida@saebti.intranet>
2023-08-21 11:45:01 -0300

    Some things added

--- app/Http/Controllers/ProfessorsController.php
+++ app/Http/Controllers/ProfessorsController.php
@@ -7,7 +7,25 @@ use Illuminate\Http\Request;
 class ProfessorsController extends Controller
 {
     public function professors(){
-        $profs = [];
+        $profs = [
+            0 => [
+                'nome' => 'Lynn Alves',
+                'status' => 'Active',
+                'cnpj' => '12.345.678/0001-00',
+                'ddd' => '71', //Salvador
+                'telefone' => null
+            ],
+
+            1 => [
+                'nome' => 'Luiz Marcio',
+                'status' => 'Active',
+                'cnpj' => '12.345.678/0001-00',
+                'ddd' => '75', //Bahia Geral
+                'telefone' => null
+            ]
+        ];
+
+        
         return view('app.professors.index', compact('profs'));
     }
 }
--- resources/views/app/professors/index.blade.php
+++ resources/views/app/professors/index.blade.php
@@ -1,6 +1,29 @@
 <h1>Professors list</h1>
 
 
+<br>
+<br>
+
+
+@isset($profs) 
+    @for($i = 0; isset($profs[$i]); $i++)
+        Professor: {{$profs[$i]['nome']}}
+        <br>
+        Status: {{$profs[$i]['status']}}
+        <br>
+        Cnpj: {{$profs[$i]['cnpj']}}
+        <br>
+        Telefone: ({{$profs[$i]['ddd'] ?? ''}}) {{$profs[$i]['telefone'] ?? 'Não informado'}}
+        <br>
+        <br>
+        <hr>   
+    @endfor    
+@endisset
+    
+
+
+
+
 
 
 @if (count($profs) > 0)
