ae02d22f83888fb941b2b0b43ae7973b7fe1e45b
Felipe Santana D`Eça Almeida <felipe.almeida@saebti.intranet>
2023-09-20 11:51:28 -0300

    migrations implemented

--- database/migrations/2023_09_19_133536_create_site_contatos_table.php
+++ database/migrations/2023_09_19_133536_create_site_contatos_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('fullname',100);
+            $table->string('emailcontact',100);
+            $table->string('subjectcontact',100);
+            $table->text('messageabout');
         });
     }
 
