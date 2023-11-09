64a10dfadc9afb6121ab2e57d5273534ce74d9b8
Gancho <vqvvoip@gmail.com>
2023-11-09 15:02:12 -0300

    login started

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,4 +9,22 @@ class LoginController extends Controller
    public function login(){
       return view('site.login');
    }
+   public function store()
+    {
+        if (auth()->attempt(request(['email', 'password'])) == false) {
+            return back();
+        }
+        
+        return redirect()->to('/home');
+    }
+    
+    public function destroy()
+    {
+        auth()->logout();
+        
+        return redirect()->to('/login');
+    }
 }
+
+
+
